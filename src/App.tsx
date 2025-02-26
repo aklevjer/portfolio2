8e8c666310bd6e21b202ada147539efdd4fdd20d
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-26 19:12:08 +0100

    Update App component with base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main className="space-y-20 py-12 md:py-20">Main</main>
+      <Footer />
+    </>
+  );
 }
