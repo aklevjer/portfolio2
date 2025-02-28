2aa6739c7c4a205c69a9948736b9b0fe483637f6
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-28 07:57:57 +0100

    Add projects constants

---
+++ src/constants/projects.ts
@@ -0,0 +1,26 @@
+export const PROJECTS = [
+  {
+    title: "Holidaze",
+    image: "/images/holidaze-screen.jpg",
+    description:
+      "An accommodation booking site where users can book and list venues. Developed for Noroff’s Project Exam 2 using React, TypeScript, and Tailwind CSS, with data powered by Noroff’s API.",
+    website: "https://holidaze-ak.netlify.app",
+    github: "https://github.com/aklevjer/holidaze-pe2",
+  },
+  {
+    title: "DirectShop",
+    image: "/images/directshop-screen.jpg",
+    description:
+      "An e-commerce site for browsing and purchasing products. Developed for Noroff’s JavaScript Frameworks course using React, Tailwind CSS, and JavaScript, with data from Noroff’s API.",
+    website: "https://directshop.netlify.app",
+    github: "https://github.com/aklevjer/js-frameworks-ca",
+  },
+  {
+    title: "BidPoint",
+    image: "/images/bidpoint-screen.jpg",
+    description:
+      "An auction site where users can list and bid on items. Developed for Noroff’s Semester Project 2 using HTML, Tailwind CSS, and JavaScript, with data managed through Noroff’s API.",
+    website: "https://bidpoint.netlify.app",
+    github: "https://github.com/aklevjer/bidpoint-sp2",
+  },
+];
