d24038c153c774f4b676c42cfcb3f63d93280a22
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-28 08:00:36 +0100

    Add Projects section component

---
+++ src/components/sections/Projects/index.tsx
@@ -0,0 +1,17 @@
+import { PROJECTS } from "@/constants/projects";
+import Heading from "@/components/ui/Heading";
+import Card from "@/components/ui/Card";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="scroll-mt-8 space-y-4">
+      <Heading level="h2">Projects</Heading>
+
+      <ul className="space-y-6">
+        {PROJECTS.map((project) => (
+          <Card key={project.title} project={project} />
+        ))}
+      </ul>
+    </section>
+  );
+}
